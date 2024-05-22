272706d1e5a6239ad38669939b4656f78a9a6315
TohaDoku <dokuchaevantoha@gmail.com>
2024-05-22 22:29:11 +0300

    настройка страницы статей

--- knowledgebase/main/views.py
+++ knowledgebase/main/views.py
@@ -24,7 +24,20 @@ def category_page(request, slug):
 def subcategories(request, category_slug, subcategory_slug):
     category = get_object_or_404(Category, slug=category_slug)
     subcategory = get_object_or_404(Subcategory, slug=subcategory_slug, category=category)
+    articles = Article.objects.filter(subcategory=subcategory)
     context = {
-        'subcategory': subcategory
+        'subcategory': subcategory,
+        'articles': articles,
+    }
+    return render(request, 'main/subcategory.html', context)
+
+
+def article_page(request, category_slug, subcategory_slug, article_slug):
+    category = get_object_or_404(Category, slug=category_slug)
+    subcategory = get_object_or_404(Subcategory, slug=article_slug)
+    article = Article.objects.get(slug=subcategory_slug)
+    context = {
+        'subcategory': subcategory,
+        'article': article,
     }
     return render(request, 'main/subcategory.html', context)
